443e0e19a27790fb37cc13bf5d9ec3c1053fd2ba
Filterrr <36611101+Filterrr@users.noreply.github.com>
2026-04-07 21:43:58 +0800

    Update adblock_rule_generator_yaml.ps1
    Signed-off-by: Filterrr <36611101+Filterrr@users.noreply.github.com>

--- adblock_rule_generator_yaml.ps1
+++ adblock_rule_generator_yaml.ps1
@@ -6,8 +6,7 @@
 
 # 定义广告过滤器URL列表
 $urlList = @(
-"https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt",  
-"https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt"
+"https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt"
 )
 
 # 日志文件路径
