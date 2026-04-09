e9f41fae57b9fcea518d6cb3e19aca2e7fadc1b0
Filterrr <36611101+Filterrr@users.noreply.github.com>
2026-04-09 21:52:33 +0800

    Update adblock_rule_generator_yaml.ps1
    Signed-off-by: Filterrr <36611101+Filterrr@users.noreply.github.com>

--- adblock_rule_generator_yaml.ps1
+++ adblock_rule_generator_yaml.ps1
@@ -6,6 +6,7 @@ $EnableRootDomainDrops = $true
 # === 配置采集源 ===
 $urlList = @(
     "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt",
+    "https://raw.githubusercontent.com/Filterrr/AdBlock_Rule_For_Clash/main/allowlist.txt",
     "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
     "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/MobileFilter/sections/adservers.txt",
