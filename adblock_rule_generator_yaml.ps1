dd5e9fdf10b7e689138c1fea27e76f48f21b35c1
Filterrr <36611101+Filterrr@users.noreply.github.com>
2026-04-14 19:20:01 +0800

    Update adblock_rule_generator_yaml.ps1
    Signed-off-by: Filterrr <36611101+Filterrr@users.noreply.github.com>

--- adblock_rule_generator_yaml.ps1
+++ adblock_rule_generator_yaml.ps1
@@ -14,7 +14,7 @@ $urlList = @(
     "https://easylist-downloads.adblockplus.org/easylistchina.txt",
     "https://easylist-downloads.adblockplus.org/easylist.txt",
     "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",
-    "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",
+    "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt"
 #    "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
 )
 
