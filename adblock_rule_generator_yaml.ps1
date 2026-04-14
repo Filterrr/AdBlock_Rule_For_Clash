412b91ba1a7f6f9e1ab1bb78e49152b3ec05b74c
Filterrr <36611101+Filterrr@users.noreply.github.com>
2026-04-14 19:24:34 +0800

    Update adblock_rule_generator_yaml.ps1
    Signed-off-by: Filterrr <36611101+Filterrr@users.noreply.github.com>

--- adblock_rule_generator_yaml.ps1
+++ adblock_rule_generator_yaml.ps1
@@ -6,7 +6,7 @@
 
 # 定义广告过滤器URL列表
 $urlList = @(
-#    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt",
+    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt",
     "https://raw.githubusercontent.com/Filterrr/AdBlock_Rule_For_Clash/main/allowlist.txt",
     "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
     "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
@@ -15,7 +15,6 @@ $urlList = @(
     "https://easylist-downloads.adblockplus.org/easylist.txt",
     "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",
     "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt"
-#    "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
 )
 
 # 日志文件路径
