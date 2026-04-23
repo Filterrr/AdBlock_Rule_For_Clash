1eeb0db7c64a2aa3e10fb15a9fd0a35f34c0fdea
Filterrr <36611101+Filterrr@users.noreply.github.com>
2026-04-23 22:42:42 +0800

    Update adblock_rule_generator_yaml.py
    Signed-off-by: Filterrr <36611101+Filterrr@users.noreply.github.com>

--- adblock_rule_generator_yaml.py
+++ adblock_rule_generator_yaml.py
@@ -26,22 +26,23 @@ allow_urls = [
 ]
 
 tier1_urls = [
-    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/MobileFilter/sections/adservers.txt"
+ #   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
+ #   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt",
+ #   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
+ #   "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/MobileFilter/sections/adservers.txt"
 ]
 
 tier2_urls = [
-    "https://easylist-downloads.adblockplus.org/easylistchina.txt",
-    "https://easylist-downloads.adblockplus.org/easylist.txt"
+  #  "https://easylist-downloads.adblockplus.org/easylistchina.txt",
+  #  "https://easylist-downloads.adblockplus.org/easylist.txt"
 ]
 
 tier3_urls = [
-    "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",
-    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt",
-    "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",
-    "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt"
+  #  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",
+  #  "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt",
+  #  "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",
+  #  "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
+    "https://johnshall.github.io/Shadowrocket-ADBlock-Rules-Forever/sr_ad_only.conf"
 ]
 
 # 获取当前运行目录的绝对路径
